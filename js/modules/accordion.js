d9166579a14a01fcb067627427ad5b58bfbca6e1
Vitosnatios <vitosdeveloper@gmail.com>
2022-09-15 21:03:17 -0300

    adicionado package-lock.json

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,5 +1,7 @@
 export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
   const activeClass = 'ativo';
 
   function activeAccordion() {
