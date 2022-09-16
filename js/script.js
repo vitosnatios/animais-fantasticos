d06af82c6ab13e2d2420bb2f8d0006d04bc68f69
Vitosnatios <vitosdeveloper@gmail.com>
2022-09-15 22:50:47 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -16,7 +16,9 @@ const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]', {
 });
 scrollSuave.init();
 
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
 initModal();
 initTooltip();
